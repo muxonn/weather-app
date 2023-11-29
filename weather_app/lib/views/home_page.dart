2235edc332ed1f16f8bcf53496eecd6b2c53f216
Jakub Madry <jaiwiecejmnie@gmail.com>
2023-11-29 01:02:51 +0100

    Add search bar to bottom

--- weather_app/lib/views/home_page.dart
+++ weather_app/lib/views/home_page.dart
@@ -18,8 +18,8 @@ class HomePage extends HookWidget {
   Widget build(BuildContext context) {
     final future = useMemoized(getCurrentWeather);
     final snapshot = useFuture(future);
+    final searchController = useTextEditingController();
 
-    //getCurrentWeather();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -30,46 +30,61 @@ class HomePage extends HookWidget {
       ),
       backgroundColor: Colors.white,
       drawer: Drawer(),
-      body: Center(
-        child: snapshot.hasData
-            ? buildCurrentWeather(snapshot.data as CurrentWeather)
-            : CircularProgressIndicator(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: snapshot.hasData
+                ? buildCurrentWeather(snapshot.data as CurrentWeather)
+                : CircularProgressIndicator(),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: SearchBar(
+              controller: searchController,
+              onSubmitted: (String value) {
+                print(searchController.text);
+              },
+              constraints: const BoxConstraints(maxWidth: 300, minHeight: 55),
+              leading: const Icon(Icons.search),
+              backgroundColor: const MaterialStatePropertyAll(Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Widget buildCurrentWeather(CurrentWeather data) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child: Column(
-        children: [
-          Text(
-            "${data.locationName}, ${data.country}",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            "${data.conditionText}",
-            style: TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "${data.temperatureCelcius?.round()}°",
-                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+    return Column(
+      children: [
+        Text(
+          "${data.locationName}, ${data.country}",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          "${data.conditionText}",
+          style: TextStyle(fontSize: 16),
+        ),
+        const SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${data.temperatureCelcius?.round()}°",
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 10),
+            Container(
+              child: Image.network(
+                "https:${data.conditionIcon}",
               ),
-              SizedBox(width: 10),
-              Container(
-                child: Image.network(
-                  "https:${data.conditionIcon}",
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
