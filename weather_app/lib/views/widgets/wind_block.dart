61745335ae8673d58f937388d2176172ca28d4bf
Jakub Madry <jaiwiecejmnie@gmail.com>
2024-02-18 02:29:49 +0100

    Change ball color and arrow direction depending on wind direction

--- weather_app/lib/views/widgets/wind_block.dart
+++ weather_app/lib/views/widgets/wind_block.dart
@@ -3,13 +3,40 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class WindBlock extends StatelessWidget {
-  const WindBlock({required this.windDirection, required this.windSpeed});
+  WindBlock({required this.windDirection, required this.windSpeed});
 
   final String windDirection;
   final double windSpeed;
 
+  // TODO: Simplify that, maybe add an enum or something
+
+  Map<String, int> directionIndex = {
+    'N': 0,
+    'NNE': 1,
+    'NE': 2,
+    'ENE': 3,
+    'E': 4,
+    'ESE': 5,
+    'SE': 6,
+    'SSE': 7,
+    'S': 8,
+    'SSW': 9,
+    'SW': 10,
+    'WSW': 11,
+    'W': 12,
+    'WNW': 13,
+    'NW': 14,
+    'NNW': 15,
+  };
+
   @override
   Widget build(BuildContext context) {
+    Map<String, double> directionRadians = {};
+
+    directionIndex.forEach((dir, index) {
+      double angle = index * (360 / 16);
+      directionRadians[dir] = angle / 180 * pi;
+    });
     return Container(
       width: 160,
       height: 150,
@@ -31,16 +58,22 @@ class WindBlock extends StatelessWidget {
                 Positioned(
                   top: 8,
                   left: 8,
-                  child: Icon(
-                    Icons.arrow_downward_rounded,
-                    size: 40,
+                  child: Transform.rotate(
+                    angle: directionRadians[windDirection]!,
+                    child: const Icon(
+                      Icons.arrow_upward_rounded,
+                      size: 40,
+                      color: Colors.red,
+                    ),
                   ),
                 ),
                 Stack(
                   children: List.generate(
                     16,
                     (index) {
-                      final double angle = (index * (360 / 16)) * (pi / 180);
+                      final double angle =
+                          ((index - 4) * (360 / 16)) * (pi / 180);
+                      print(angle);
                       final double radius = 25;
 
                       final double dotX = radius + radius * cos(angle);
@@ -54,7 +87,9 @@ class WindBlock extends StatelessWidget {
                           height: 6,
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                            color: Colors.black,
+                            color: directionIndex[windDirection] == index
+                                ? Colors.red
+                                : Colors.black,
                           ),
                         ),
                       );
